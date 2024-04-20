a1f70331fcac53c0f46291369b489f14af4475dc
khadija gull <khadija gull 2000@gmail.com>
2024-04-21 04:08:55 +0500

    first commit

---
+++ 4 assignment/main.js
@@ -0,0 +1,4 @@
+"use strict";
+let quote = "Nothing is impossible because impossible itself I Am Possible";
+let author = "Khadija Gull";
+console.log(`${quote} once said ${author}`);
---
+++ 4 assignment/main.ts
@@ -0,0 +1,6 @@
+let quote:string = "Nothing is impossible because impossible itself I Am Possible";
+
+let author:string = "Khadija Gull";
+
+
+console.log(`${quote} once said ${author}`);
