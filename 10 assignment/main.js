582fc0d4cfef8a6220302c9917d59a190c5765d3
khadija gull <khadija gull 2000@gmail.com>
2024-05-15 00:30:57 +0500

    first commit

---
+++ 10 assignment/main.js
@@ -0,0 +1,9 @@
+"use strict";
+// in this program i add two number and print it
+// define two numbers
+let num1 = 50;
+let num2 = 10;
+// add two numbers
+let sum = num1 + num2;
+// print sum
+console.log(sum);
---
+++ 10 assignment/main.ts
@@ -0,0 +1,12 @@
+// in this program i add two number and print it
+
+// define two numbers
+let num1:number = 50;
+let num2:number = 10;
+
+// add two numbers
+let sum:number = num1 + num2;
+
+// print sum
+
+console.log(sum);
