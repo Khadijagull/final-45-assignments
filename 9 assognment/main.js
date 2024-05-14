0d3a3289423c50e4a53b7c98bd8f83ae0bb23d88
khadija gull <khadija gull 2000@gmail.com>
2024-05-14 20:12:53 +0500

    first commit

---
+++ 9 assognment/main.js
@@ -0,0 +1,4 @@
+"use strict";
+let favoriteNum = 7;
+let massage = `My favorite number is ${favoriteNum}`;
+console.log(massage);
---
+++ 9 assognment/main.ts
@@ -0,0 +1,5 @@
+let favoriteNum:number = 7;
+
+let massage:string = `My favorite number is ${favoriteNum}`;
+
+console.log(massage);
