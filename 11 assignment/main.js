974a3de4c5b5e5a9b0a5a35df78a07c1707251eb
khadija gull <khadija gull 2000@gmail.com>
2024-05-15 17:58:47 +0500

    first commit

---
+++ 11 assignment/main.js
@@ -0,0 +1,6 @@
+"use strict";
+let myFriendNames = ["Nayab", "Reeha", "Hira", "Naosheen", "Hafsa"];
+for (let i = 0; i < myFriendNames.length; i++) {
+    console.log(myFriendNames[i]);
+}
+;
---
+++ 11 assignment/main.ts
@@ -0,0 +1,6 @@
+let myFriendNames:string[] = ["Nayab" , "Reeha" , "Hira" , "Naosheen" , "Hafsa"];
+
+for (let i = 0; i < myFriendNames.length; i++) {
+
+    console.log(myFriendNames[i])
+};
