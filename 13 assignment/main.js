d7104df9d54b7afb2a7474ac21bb4e818d5ea771
khadija gull <khadija gull 2000@gmail.com>
2024-05-15 19:38:14 +0500

    first commit

---
+++ 13 assignment/main.js
@@ -0,0 +1,14 @@
+"use strict";
+let transportationMode = ["bike", "car", "motorcycle", "jet"];
+// // loop
+for (let i = 0; i < transportationMode.length; i++) {
+    console.log(`i would like to own ${transportationMode[i]}`);
+}
+// for each
+transportationMode.forEach(names => {
+    console.log(`i would like to own ${names}`);
+});
+//  for of loop
+for (let names of transportationMode) {
+    console.log(`i would like to own ${names}`);
+}
