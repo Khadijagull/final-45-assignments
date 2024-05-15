b80f67d62800db5691b637806725eac95753a527
khadija gull <khadija gull 2000@gmail.com>
2024-05-15 21:51:33 +0500

    first commit

---
+++ 14 assignment/main.js
@@ -0,0 +1,5 @@
+var guestList = ["Ayesha baji", "Duaa baji", "Rimsha baji", "Marium baji"];
+//  for each
+guestList.forEach(function (guestName) {
+    console.log("Sister ".concat(guestName, ", you are cordially invited to dinner."));
+});
---
+++ 14 assignment/main.ts
@@ -0,0 +1,6 @@
+ let guestList:string [] = ["Ayesha baji", "Duaa baji", "Rimsha baji", "Marium baji" ];
+
+//  for each
+guestList.forEach(guestName => {
+    console.log(`Sister ${guestName}, you are cordially invited to dinner.`);
+})
